3c938e27e1a298ec4748d4e49998f7d521bc7180
N1CKZ3V <nico_03_sebas@hotmail.com>
2025-02-02 18:53:25 -0500

    Configuracio y estructura incial

---
+++ src/test/java/eci/ieti/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.ieti.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
