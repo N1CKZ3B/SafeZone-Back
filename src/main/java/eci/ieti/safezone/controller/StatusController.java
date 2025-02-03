3d72de05ae93323d226296020e18ccd09ac289d8
juli <bricenojuliana@gmail.com>
2025-02-02 20:50:00 -0500

    Cambio de version de Java 17 a Java 21. Creación de directorios básicos para controladores y excepciones. Creación de endpoint básico para verificar el estado del servidor en /status.

---
+++ src/main/java/eci/ieti/safezone/controller/StatusController.java
@@ -0,0 +1,13 @@
+package eci.ieti.safezone.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public String getStatus() {
+        return "The app is running";
+    }
+}
---
+++ src/main/java/eci/ieti/safezone/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package eci.ieti.safezone.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Manejo de excepciones generales
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGlobalException(Exception ex, WebRequest request) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("message", ex.getMessage());
+        response.put("details", request.getDescription(false));
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
